b930e8042784241c2ce8122e4492cf9e24c09581
7rodo <7rodovje7878@gmail.com>
2020-08-07 00:26:15 +0100

    Update source-ship.js

--- scripts/source-ship.js
+++ scripts/source-ship.js
@@ -42,7 +42,7 @@ const sourceShip = extendContent(Mech, "source-ship", {
   },
 
   updateAlt(player){
-    if (!Vars.state.isPaused()) Effects.effect(sourceTrail, Mathf.random(player.drawx()-5, player.drawx()+5), Mathf.random(player.drawy()-5, player.drawy()+5) );
+    if (!Vars.state.isPaused()) Effects.effect(sourceTrail, Mathf.random(player.x()-5, player.x()+5), Mathf.random(player.y()-5, player.y()+5) );
   }
 });
 
