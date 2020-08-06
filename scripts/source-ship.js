1801449125fa8b542350878842c2cd2daeda6546
7rodo <7rodovje7878@gmail.com>
2020-08-07 00:01:53 +0100

    Update source-ship.js

--- scripts/source-ship.js
+++ scripts/source-ship.js
@@ -2,8 +2,8 @@ const sourceTrail = newEffect(35, e => {
    // var offset = 0.3 + e.fin() * 0.4;
  //   var angle = Mathf.randomSeed(e.id, 360);
     Draw.alpha(Mathf.random(0.3, 0.5));
-    Draw.color(Pal.lancerLaser);
-    Lines.circle(e.x, e.y, e.fout() * 3.4);
+    Draw.color(Pal.plastaniumBack);
+    Lines.tri(e.x, e.y, e.fout() * 1);
   //  Lines.circle(e.x + Angles.trnsx(angle, offset), e.y + Angles.trnsy(angle, offset), e.rotation - 90);
 });
 
