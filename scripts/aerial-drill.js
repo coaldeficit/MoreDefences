4edf39ac949259085f60ba55ccecc5907d1eddd7
7rodo <7rodovje7878@gmail.com>
2020-08-10 18:43:51 +0100

    aerial drill

---
+++ scripts/aerial-drill.js
@@ -0,0 +1,25 @@
+const aerialdrill = extendContent(Drill, "aerial-drill", {
+  load(){
+    this.region = Core.atlas.find(this.name);
+    this.propellorRegion = Core.atlas.find(this.name + "-propellor");
+    this.rotateRegion = Core.atlas.find(this.name + "-rotator");
+    this.topRegion = Core.atlas.find(this.name + "-top");
+  },
+  
+  generateIcons(){
+  return [
+    Core.atlas.find(this.name),
+    Core.atlas.find(this.name + "-propellor"),
+    Core.atlas.find(this.name + "-rotator"),
+    Core.atlas.find(this.name + "-top")
+  ];},
+   
+  draw(tile){
+    entity = tile.ent();
+    
+    Draw.rect(this.region, tile.drawx(), tile.drawy());
+    Draw.rect(this.propellorRegion, tile.drawx(), tile.drawy(), Time.time() * -3 * this.efficiency);
+    Draw.rect(this.rotateRegion, tile.drawx(), tile.drawy(), Time.time()* 8 * this.efficiency);
+    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+  }
+});
