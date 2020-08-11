f964bd24e1e3ed7573439d71f74611b3b97e52b4
7rodo <7rodovje7878@gmail.com>
2020-08-11 22:22:40 +0100

    Update aerial-drill.js

--- scripts/aerial-drill.js
+++ scripts/aerial-drill.js
@@ -2,7 +2,7 @@ const aerialdrill = extendContent(Drill, "aerial-drill", {
   load(){
     this.super$load();
     this.region = Core.atlas.find(this.name);
-    this.rimRegion = Core.atlas.find(this.name + '-rim');
+ //   this.rimRegion = Core.atlas.find(this.name + '-rim');
     this.propellorRegion = Core.atlas.find(this.name + "-propellor");
     this.rotateRegion = Core.atlas.find(this.name + "-rotator");
     this.topRegion = Core.atlas.find(this.name + "-top");
@@ -20,13 +20,6 @@ const aerialdrill = extendContent(Drill, "aerial-drill", {
     entity = tile.ent();
     
     Draw.rect(this.region, tile.drawx(), tile.drawy());
-    
-    Draw.color(Color.valueOf('000000'), Color.valueOf('ffc2d6'), entity.getHeat() * (0.7 + Mathf.absin(Time.time(), 3, 0.3)));
-    Draw.blend(Blending.additive);
-    Draw.rect(this.rimRegion, tile.drawx(), tile.drawy());
-    Draw.blend();
-    Draw.color();
-    
     Draw.rect(this.propellorRegion, tile.drawx(), tile.drawy(), Time.time() * 8 * entity.efficiency());    
     Draw.rect(this.rotateRegion, tile.drawx(), tile.drawy(), Time.time() * -6 * entity.efficiency());
     Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
