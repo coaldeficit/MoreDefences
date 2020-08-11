bc2163d72b5873e19ec0809d52d815d7a82b0926
7rodo <7rodovje7878@gmail.com>
2020-08-11 01:54:14 +0100

    Update aerial-drill.js

--- scripts/aerial-drill.js
+++ scripts/aerial-drill.js
@@ -23,8 +23,10 @@ const aerialdrill = extendContent(Drill, "aerial-drill", {
     Draw.rect(this.rimRegion, tile.drawx(), tile.drawy());
     Draw.blend();
     Draw.color();
+    if (entity.power.status > 0.01 && entity.liquids.total() > 0.01) {
     Draw.rect(this.propellorRegion, tile.drawx(), tile.drawy(), Time.time() * 8 * entity.efficiency());    
     Draw.rect(this.rotateRegion, tile.drawx(), tile.drawy(), Time.time() * -6 * entity.efficiency());
     Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+    }
   }
 });
