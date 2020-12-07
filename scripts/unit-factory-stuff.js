ea911e45cd5c4d67f3822c8ccbde896a480ac649
7rodo <7rodovje7878@gmail.com>
2020-12-07 22:58:29 +0000

    Delete unit-factory-stuff.js

--- scripts/unit-factory-stuff.js
+++
@@ -1,7 +0,0 @@
-// air factory
-Blocks.airFactory.plans.add(new UnitFactory.UnitPlan(more-defences-ivyx-arrow-ship, 60 * 25, ItemStack.with(Items.silicon, 25, Items.lead, 10)));
-
-// ground factory
-
-
-// naval factory
