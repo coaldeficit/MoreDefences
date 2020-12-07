3cf5f16008f8793ee0ac7bba073635b2a125d1b4
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2020-12-07 22:29:57 +0100

    Update unit-factory-stuff.js

--- scripts/unit-factory-stuff.js
+++ scripts/unit-factory-stuff.js
@@ -1,5 +1,5 @@
 // air factory
-Blocks.airFactory.plans.add(new UnitFactory.UnitPlan("arrow-ship", 60 * 25, ItemStack.with(Items.silicon, 25, Items.lead, 10)));
+Blocks.airFactory.plans.add(new UnitFactory.UnitPlan(arrow-ship, 60 * 25, ItemStack.with(Items.silicon, 25, Items.lead, 10)));
 
 // ground factory
 
