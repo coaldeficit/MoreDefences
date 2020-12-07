678f8dad759f7749b0ec69c5f856d84be1ae742e
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2020-12-07 20:39:17 +0100

    Create unit-factory-stuff.js

---
+++ scripts/unit-factory-stuff.js
@@ -0,0 +1,7 @@
+// air factory
+Blocks.airFactory.plans.add(new UnitFactory.UnitPlan("arrow-ship", 60 * 25, ItemStack.with(Items.silicon, 25, Items.lead, 10)));
+
+// ground factory
+
+
+// naval factory
