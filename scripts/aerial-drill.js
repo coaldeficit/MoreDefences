a12ca694854527a8235d91cf9eae160b80e7a390
7rodo <7rodovje7878@gmail.com>
2020-12-09 12:13:18 +0000

    Update aerial-drill.js

--- scripts/aerial-drill.js
+++ scripts/aerial-drill.js
@@ -18,8 +18,8 @@ const aerial = extendContent(Drill, "aerial-drill", {
 aerial.buildType = () => extend(Drill.DrillBuild, aerial, {
   draw(){    
     Draw.rect(aerial.region, this.x, this.y);
-    Draw.rect(aerial.propellorRegion, this.x, this.y, this.timeDrilled * this.rotateSpeed);
-    Draw.rect(aerial.rotateRegion, this.x, this.y, this.timeDrilled * this.rotateSpeed);
+    Draw.rect(aerial.propellorRegion, this.x, this.y, this.timeDrilled * aerial.rotateSpeed);
+    Draw.rect(aerial.rotateRegion, this.x, this.y, this.timeDrilled * aerial.rotateSpeed);
     Draw.rect(aerial.topRegion, this.x, this.y);
   }
 });
