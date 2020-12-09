51f90164ea46ef44acfd26a24f648dd659235f66
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2020-12-09 02:16:10 +0100

    Update factories.js

--- scripts/factories.js
+++ scripts/factories.js
@@ -1,3 +1,3 @@
-const cannoneerT1 = extendContent(UnitType, "cannoneer-mech", {});
+// const cannoneerT1 = extendContent(UnitType, "cannoneer-mech", {});
 
 Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(cannoneerT1, 60 * 27, ItemStack.with(Items.silicon, 15, Items.graphite, 25)));
