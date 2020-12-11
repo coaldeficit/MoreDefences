af95cc74332aa353095f4be18a953bbf4f41f30a
7rodo <7rodovje7878@gmail.com>
2020-12-11 12:11:39 +0000

    Update factories.js

--- scripts/factories.js
+++ scripts/factories.js
@@ -1,5 +1,4 @@
-const ais = this.global.mcu.ai;
-
 const CannonT1 = extendContent(UnitType, "cannoneer-mech", {});
-
-Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(CannonT1, 60 * 30, ItemStack.with(Items.silicon, 15, Items.graphite, 25)));
+CannonT1.constructor = () => extend(UnitEntity, {});
+//CannonT1.abilities.add(new MoveLightningAbility(15, 8, 0.1, 1, 3.1, Color.valueOf("#a9d8ff")));
+Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(CannonT1, 60 * 25, ItemStack.with(Items.silicon, 15, Items.graphite, 30)));
