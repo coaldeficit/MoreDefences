0f0bac07a8a83776925da9a581c3927ae43209c1
7rodo <7rodovje7878@gmail.com>
2020-12-11 19:22:57 +0000

    Update factories.js

--- scripts/factories.js
+++ scripts/factories.js
@@ -3,4 +3,4 @@ const CannonT1 = extendContent(UnitType, "cannoneer-mech", {});
 CannonT1.constructor = () => extend(UnitType, {});
 Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(CannonT1, 60 * 25, ItemStack.with(Items.silicon, 15, Items.graphite, 30)));
 
-var CannonT2 = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-cannoneer-mech"), Vars.content.getByName(ContentType.unit, "more-defences-ivyx-artilleryman-mech")]);
+//var CannonT2 = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-cannoneer-mech"), Vars.content.getByName(ContentType.unit, "more-defences-ivyx-artilleryman-mech")]);
