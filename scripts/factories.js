cc8aae5125f295f044f40ff2587fd2d60812c17b
7rodo <7rodovje7878@gmail.com>
2020-12-11 19:06:51 +0000

    Update factories.js

--- scripts/factories.js
+++ scripts/factories.js
@@ -1,4 +1,6 @@
+// Cannoneer
 const CannonT1 = extendContent(UnitType, "cannoneer-mech", {});
-CannonT1.constructor = () => extend(UnitEntity, {});
-//CannonT1.abilities.add(new MoveLightningAbility(15, 8, 0.1, 1, 3.1, Color.valueOf("#a9d8ff")));
+CannonT1.constructor = () => extend(UnitType, {});
 Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(CannonT1, 60 * 25, ItemStack.with(Items.silicon, 15, Items.graphite, 30)));
+
+var CannonT2 = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-cannoneer-mech"), Vars.content.getByName(ContentType.unit, "more-defences-ivyx-artilleryman-mech")]);
