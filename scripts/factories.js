fe759644665ff3aef9388509326e31f7daa7692e
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2020-12-12 15:13:32 +0100

    Update factories.js

--- scripts/factories.js
+++ scripts/factories.js
@@ -3,4 +3,11 @@ const CannonT1 = extendContent(UnitType, "cannoneer-mech", {});
 CannonT1.constructor = () => extend(UnitEntity, {});
 Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(CannonT1, 60 * 25, ItemStack.with(Items.silicon, 15, Items.graphite, 30)));
 
+  // Artilleryman
 //var CannonT2 = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-cannoneer-mech"), Vars.content.getByName(ContentType.unit, "more-defences-ivyx-artilleryman-mech")]);
+
+
+// Swarmer
+const SwarmT1 = extendContent(UnitType, "swarmer-ship", {});
+CannonT1.constructor = () => extend(UnitEntity, {});
+Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(SwarmT1, 60 * 25, ItemStack.with(Items.silicon, 15, Items.graphite, 30)));
