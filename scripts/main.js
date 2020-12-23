ff69741d1655b0167a851e2a181a6e509d752298
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2020-12-23 14:44:15 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,5 +4,7 @@ require("more-defences-ivyx/vanadium-megaextractor")
 // require("more-defences-ivyx/tefium-wall")
 // require("more-defences-ivyx/source-ship")
 require("more-defences-ivyx/aerial-drill")
+
+// unit stuff
 require("more-defences-ivyx/units/ground/cannoneer/cannoneer")
 require("more-defences-ivyx/units/ground/cannoneer/artilleryman")
