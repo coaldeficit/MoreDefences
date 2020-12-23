a9847615c2de563ec34a9cbb35389a516cff6e20
7rodo <7rodovje7878@gmail.com>
2020-12-23 13:38:10 +0000

    Update cannoneer.js

--- scripts/units/ground/cannoneer/cannoneer.js
+++ scripts/units/ground/cannoneer/cannoneer.js
@@ -1,5 +1,5 @@
-const CannonT1 = extendContent(UnitType, "cannoneer-mech", {});
+const CannonT1 = extendContent(UnitType, "canoneer-mech", {});
 
 CannonT1.constructor = () => extend(UnitType, {});
 
-Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(CannonT1, 60 * 25, ItemStack.with(Items.silicon, 45, Items.metaglass, 25, Items.titanium, 20)));
+Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(CannonT1, 60 * 25, ItemStack.with(Items.silicon, 30, Items.graphite, 15)));
