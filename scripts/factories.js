bad78617080d2939327f9fbb0a414a36fe478b60
7rodo <7rodovje7878@gmail.com>
2020-12-23 00:06:27 +0000

    Update factories.js

--- scripts/factories.js
+++ scripts/factories.js
@@ -7,8 +7,8 @@ CannonT1.constructor = () => extend(UnitEntity, {});
 Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(CannonT1, 60 * 25, ItemStack.with(Items.silicon, 15, Items.graphite, 30)));
 
 // Artilleryman
-var cannont2upgrade = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-cannoneer-mech"), Vars.content.getByName(ContentType.unit, "more-defences-ivyx-artilleryman-mech")]);
-Blocks.additiveReconstructor.upgrades.add(cannont2upgrade.toArray(UnitType));
+var CannonT2 = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-cannoneer-mech"), Vars.content.getByName(ContentType.unit, "more-defences-ivyx-artilleryman-mech")]);
+Blocks.additiveReconstructor.upgrades.add(CannonT2.toArray(UnitType));
 
 
 // Air Units
