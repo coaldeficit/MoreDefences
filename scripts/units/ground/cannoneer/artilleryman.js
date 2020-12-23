93ff9af08460831fbeeded50d50813b197b535c4
7rodo <7rodovje7878@gmail.com>
2020-12-23 13:47:09 +0000

    Update artilleryman.js

--- scripts/units/ground/cannoneer/artilleryman.js
+++ scripts/units/ground/cannoneer/artilleryman.js
@@ -2,6 +2,6 @@ const CannonT2 = extendContent(UnitType, "artilleryman-mech", {});
 
 CannonT2.constructor = () => extend(UnitType, {});
 
-var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx"), Vars.content.getByName(ContentType.unit, "more-defences-artilleryman-mech")]);
+var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-cannoneer-mech"), Vars.content.getByName(ContentType.unit, "more-defences-artilleryman-mech")]);
 
 Blocks.additiveReconstructor.upgrades.add(upgrade.toArray(UnitType));
