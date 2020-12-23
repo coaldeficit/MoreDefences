3bc8e2995823f74edfb1eb1746ba8251a78b50a4
7rodo <7rodovje7878@gmail.com>
2020-12-23 13:17:13 +0000

    Update and rename e.txt to cannoneer.js

---
+++ scripts/units/ground/cannoneer/cannoneer.js
@@ -0,0 +1,23 @@
+const UnitPlan = UnitFactory.UnitPlan;
+// Ground Units
+
+// Cannoneer Line
+// Cannoneer
+const CannonT1 = extendContent(UnitType, "cannoneer-mech", {});
+CannonT1.constructor = () => extend(UnitEntity, {});
+Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(CannonT1, 60 * 25, ItemStack.with(Items.silicon, 15, Items.graphite, 30)));
+
+// Artilleryman
+ var CannonT2 = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-cannoneer-mech"), Vars.content.getByName(ContentType.unit, "more-defences-ivyx-artilleryman-mech")]);
+ Blocks.additiveReconstructor.upgrades.add(CannonT2.toArray(UnitType));
+
+
+// Air Units
+
+// Swarmer Line
+// Swarmer
+const SwarmT1 = extendContent(UnitType, "swarmer-ship", {});
+SwarmT1.constructor = () => extend(UnitEntity, {});
+Blocks.airFactory.plans.add(new UnitFactory.UnitPlan(SwarmT1, 60 * 5, ItemStack.with(Items.silicon, 5, Items.copper, 5)));
+
+--------------------------------
