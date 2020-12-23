aaeba6b8c7eecd443ceb82d4c229c28fd5c3dae3
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2020-12-23 14:57:35 +0100

    Update artilleryman.js

--- scripts/units/ground/cannoneer/artilleryman.js
+++ scripts/units/ground/cannoneer/artilleryman.js
@@ -1,6 +1,6 @@
 const CannonT2 = extendContent(UnitType, "artilleryman-mech", {});
 
-CannonT2.constructor = () => extend(UnitType, {});
+CannonT2.constructor = () => extend(UnitEntity, {});
 
 var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-cannoneer-mech"), Vars.content.getByName(ContentType.unit, "more-defences-artilleryman-mech")]);
 
