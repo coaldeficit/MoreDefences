61a9ec7cf3a390305a5cb78d5d1f20c1696fbd08
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2020-12-24 02:34:50 +0100

    Create ordnance.js

---
+++ scripts/units/ground/cannoneer/ordnance.js
@@ -0,0 +1,5 @@
+const CannonT3 = extendContent(UnitType, "ordnance-mech", {});
+CannonT3.constructor = () => extend(UnitEntity, {});
+
+var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-artilleryman-mech"), Vars.content.getByName(ContentType.unit, "more-defences-ivyx-ordnance-mech")]);
+Blocks.multiplicativeReconstructor.upgrades.add(upgrade.toArray(UnitType));
