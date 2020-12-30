d0fcf333dfa6df2653545a543f5028cc56bdea5e
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2020-12-30 22:13:37 +0100

    Update bee.js

--- scripts/units/air/swarmer/bee.js
+++ scripts/units/air/swarmer/bee.js
@@ -1,5 +1,5 @@
 const SwarmerT2 = extendContent(UnitType, "bee-ship", {});
-SwarmerT2.constructor = () => extend(UnitType, {});
+SwarmerT2.constructor = () => extend(UnitEntity, {});
 
 var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-swarmer-ship"), Vars.content.getByName(ContentType.unit, "more-defences-ivyx-bee-ship")]);
 Blocks.additiveReconstructor.upgrades.add(upgrade.toArray(UnitType));
