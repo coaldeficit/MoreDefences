b9911eab152cef3a9d524f6ff53037f6fe02f9e8
7rodo <7rodovje7878@gmail.com>
2020-12-30 20:52:20 +0000

    Create bee.js

---
+++ scripts/units/air/swarmer/bee.js
@@ -0,0 +1,5 @@
+const SwarmerT2 = extendContent(UnitType, "bee-ship", {});
+SwarmerT2.constructor = () => extend(MechUnit, {});
+
+var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-swarmer-ship"), Vars.content.getByName(ContentType.unit, "more-defences-ivyx-bee-ship")]);
+Blocks.additiveReconstructor.upgrades.add(upgrade.toArray(UnitType));
