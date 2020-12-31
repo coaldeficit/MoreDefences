d4726298aedde3e0d82e5f70128560c089b4f1fc
7rodo <7rodovje7878@gmail.com>
2020-12-31 09:57:25 +0000

    Update swarmer.js

--- scripts/units/air/swarmer/swarmer.js
+++ scripts/units/air/swarmer/swarmer.js
@@ -1,3 +1,3 @@
 const SwarmT1 = extendContent(UnitType, "swarmer-ship", {});
 SwarmT1.constructor = () => extend(UnitEntity, {});
-Blocks.airFactory.plans.add(new UnitFactory.UnitPlan(SwarmT1, 60 * 15, ItemStack.with(Items.silicon, 30, Items.pyratite, 15)));
+Blocks.airFactory.plans.add(new UnitFactory.UnitPlan(SwarmT1, 60 * 15, ItemStack.with(Items.silicon, 10, Items.pyratite, 5)));
