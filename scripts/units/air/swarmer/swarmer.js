397d832a23ebe3a911b276e5e1eb6cb7a477d693
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-01-06 19:08:21 +0100

    suicideai doesnt work on air units apparently

--- scripts/units/air/swarmer/swarmer.js
+++ scripts/units/air/swarmer/swarmer.js
@@ -1,4 +1,4 @@
 const SwarmT1 = extendContent(UnitType, "swarmer-ship", {});
 SwarmT1.constructor = () => extend(UnitEntity, {});
-SwarmT1.defaultController = () => extend(SuicideAI, {});
+// SwarmT1.defaultController = () => extend(SuicideAI, {});
 Blocks.airFactory.plans.add(new UnitFactory.UnitPlan(SwarmT1, 60 * 15, ItemStack.with(Items.silicon, 10, Items.pyratite, 5)));
