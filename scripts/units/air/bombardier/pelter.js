cdaaf917975b3bac0b85427764f43f2a01652a44
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-01-06 14:42:41 +0100

    Create pelter.js

---
+++ scripts/units/air/bombardier/pelter.js
@@ -0,0 +1,6 @@
+  
+const BomberT2 = extendContent(UnitType, "pelter-ship", {});
+BomberT2.constructor = () => extend(UnitEntity, {});
+
+var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-bombardier-ship"), Vars.content.getByName(ContentType.unit, "more-defences-ivyx-pelter-ship")]);
+Blocks.additiveReconstructor.upgrades.add(upgrade.toArray(UnitType));
