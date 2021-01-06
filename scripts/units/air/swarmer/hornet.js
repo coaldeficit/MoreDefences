bbd1668b04c6706d1955078d05f4b580c9bdc000
7rodo <7rodovje7878@gmail.com>
2021-01-06 12:00:23 +0000

    fixed

--- scripts/units/air/swarmer/hornet.js
+++ scripts/units/air/swarmer/hornet.js
@@ -2,4 +2,4 @@ const SwarmerT3 = extendContent(UnitType, "hornet-ship", {});
 SwarmerT3.constructor = () => extend(UnitEntity, {});
 
 var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-bee-ship"), Vars.content.getByName(ContentType.unit, "more-defences-ivyx-hornet-ship")]);
-Blocks.additiveReconstructor.upgrades.add(upgrade.toArray(UnitType));
+Blocks.multiplicativeReconstructor.upgrades.add(upgrade.toArray(UnitType));
