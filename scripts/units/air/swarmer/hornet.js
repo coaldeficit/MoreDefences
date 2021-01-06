f3a15af3ba7081b6e6872af8eba517629f3afc26
7rodo <7rodovje7878@gmail.com>
2021-01-06 11:55:26 +0000

    Create hornet.js

---
+++ scripts/units/air/swarmer/hornet.js
@@ -0,0 +1,5 @@
+const SwarmerT3 = extendContent(UnitType, "hornet-ship", {});
+SwarmerT3.constructor = () => extend(UnitEntity, {});
+
+var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-bee-ship"), Vars.content.getByName(ContentType.unit, "more-defences-ivyx-hornet-ship")]);
+Blocks.additiveReconstructor.upgrades.add(upgrade.toArray(UnitType));
