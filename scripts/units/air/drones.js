479424c3b5057bca6fd9338449fd8cf8552029ef
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-01-13 22:49:25 +0100

    Update drones.js

--- scripts/units/air/drones.js
+++ scripts/units/air/drones.js
@@ -1,6 +1,3 @@
-const droneFactory = extendContent(UnitFactory, "drone-factory", {});
-
 const SpiritDrone = extendContent(UnitType, "spirit-drone", {});
 SpiritDrone.constructor = () => extend(UnitEntity, {});
 SpiritDrone.defaultController = () => extend(RepairAI, {});
-droneFactory.plans.add(new UnitFactory.UnitPlan(SpiritDrone, 60 * 7, ItemStack.with(Items.silicon, 8, Items.titanium, 5)));
