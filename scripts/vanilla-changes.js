733630ca1f08449ed2d83333487240d39ed14e3c
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-01-18 23:28:57 +0100

    Update vanilla-changes.js

--- scripts/vanilla-changes.js
+++ scripts/vanilla-changes.js
@@ -8,4 +8,4 @@ fuseIron.width = 17;
 fuseIron.reloadMultiplier = 0.65;
 fuseIron.toColor = '#afafaf';
 
-Blocks.fuse.ammo.push(['more-defences-ivyx-ferum', fuseIron]);
+Blocks.fuse.ammo.push('more-defences-ivyx-ferum', fuseIron);
