e35de2ffd612f58d03926e691ede44b8b6d15a57
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-01-18 23:28:40 +0100

    Update vanilla-changes.js

--- scripts/vanilla-changes.js
+++ scripts/vanilla-changes.js
@@ -2,10 +2,10 @@
 const fuseIron = extend(ShrapnelBulletType, {
 });
 fuseIron.length = Blocks.fuse.range + 10;
-fuseIron.damage = 120f;
-fuseIron.ammoMultiplier = 3f;
-fuseIron.width = 17f;
-fuseIron.reloadMultiplier = 0.65f;
+fuseIron.damage = 120;
+fuseIron.ammoMultiplier = 3;
+fuseIron.width = 17;
+fuseIron.reloadMultiplier = 0.65;
 fuseIron.toColor = '#afafaf';
 
 Blocks.fuse.ammo.push(['more-defences-ivyx-ferum', fuseIron]);
