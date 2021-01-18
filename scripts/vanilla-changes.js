bd57906f4a34fc35d555089af99c45f2b0ee6c76
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-01-18 23:23:00 +0100

    Create vanilla-changes.js

---
+++ scripts/vanilla-changes.js
@@ -0,0 +1,11 @@
+// more ammo variety for fuse
+const fuseIron = extend(ShrapnelBulletType, {
+});
+fuseIron.length = Blocks.fuse.range + 10;
+fuseIron.damage = 120f;
+fuseIron.ammoMultiplier = 3f;
+fuseIron.width = 17f;
+fuseIron.reloadMultiplier = 0.65f;
+fuseIron.toColor = '#afafaf';
+
+Blocks.fuse.ammo.push(Items.more-defences-ivyx-ferum, fuseIron);
