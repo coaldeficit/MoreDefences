b803201d0280f3d83e4dc5e70d9dc9d5b9924ffd
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-02-02 20:42:14 +0100

    Update ordnance.js

--- scripts/units/ground/cannoneer/ordnance.js
+++ scripts/units/ground/cannoneer/ordnance.js
@@ -1,5 +1,6 @@
 const CannonT3 = extendContent(UnitType, "ordnance-mech", {});
 CannonT3.constructor = () => extend(LegsUnit, {});
+CannonT3.immunities.add(StatusEffects.corrosion);
 
 var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-artilleryman-mech"), Vars.content.getByName(ContentType.unit, "more-defences-ivyx-ordnance-mech")]);
 Blocks.multiplicativeReconstructor.upgrades.add(upgrade.toArray(UnitType));
