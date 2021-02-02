650065dcbd752ea270653cbf66998ca35548609f
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-02-02 20:41:42 +0100

    Update cannoneer.js

--- scripts/units/ground/cannoneer/cannoneer.js
+++ scripts/units/ground/cannoneer/cannoneer.js
@@ -1,5 +1,6 @@
 const CannonT1 = extendContent(UnitType, "cannoneer-mech", {});
 
 CannonT1.constructor = () => extend(MechUnit, {});
+CannonT1.immunities.add(StatusEffects.corrosion);
 
 Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(CannonT1, 60 * 25, ItemStack.with(Items.silicon, 30, Items.graphite, 15)));
