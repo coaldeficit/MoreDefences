69d1eca0bf2eb7651c81101ca569e7a134c97cd8
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-02-23 20:39:29 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ require("more-defences-ivyx/vanadium-megaextractor")
 // require("more-defences-ivyx/source-ship")
 require("more-defences-ivyx/aerial-drill")
 require("more-defences-ivyx/units/air/drones")
+require("more-defences-ivyx/units/air/core-units")
 require("more-defences-ivyx/iron-ore-extractor")
 // require("more-defences-ivyx/vanilla-changes")
 
