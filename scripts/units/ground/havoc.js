33a8c3a44c9f57163d632d3407e41818dc088562
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-03-29 23:57:54 +0200

    e

--- scripts/units/ground/havoc.js
+++
@@ -1,3 +0,0 @@
-const Havoc = extendContent(UnitType, "havoc", {});
-Havoc.constructor = () => extend(MechUnit, {});
-Havoc.immunities = [StatusEffects.unmoving, StatusEffects.disarm, StatusEffects.slow];
