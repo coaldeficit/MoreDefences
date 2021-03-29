7e08be96b192ee250cba74204615f8f70f19dc93
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-03-29 23:53:47 +0200

    Update havoc.js

--- scripts/units/ground/havoc.js
+++ scripts/units/ground/havoc.js
@@ -1,5 +1,3 @@
 const Havoc = extendContent(UnitType, "havoc", {});
 Havoc.constructor = () => extend(MechUnit, {});
-Havoc.immunities.add(StatusEffects.unmoving);
-Havoc.immunities.add(StatusEffects.disarm);
-Havoc.immunities.add(StatusEffects.slow); // test immunity to see if the immunity stuff actually works
+Havoc.immunities = [StatusEffects.unmoving, StatusEffects.disarm, StatusEffects.slow];
