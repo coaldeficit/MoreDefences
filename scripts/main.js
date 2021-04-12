55ce7bd3fd2ae85123766ad36bd584362f39099f
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-04-12 18:26:32 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ require("more-defences-ivyx/units/air/drones")
 require("more-defences-ivyx/units/air/core-units")
 require("more-defences-ivyx/zincite-extractor")
 require("more-defences-ivyx/asphalt-mixer")
-require("more-defences-ivyx/asphalt-wall")
+// require("more-defences-ivyx/asphalt-wall")
 
 // unit stuff
 // cannoneer line
