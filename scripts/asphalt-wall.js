01645f54097a47a88e2dcc49057a0a0cf8afb89a
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-04-12 16:29:15 +0200

    Create asphalt-wall.js

---
+++ scripts/asphalt-wall.js
@@ -0,0 +1,20 @@
+const wall = extendContent(Wall, "asphalt-wall", {
+  load(){
+    this.region = Core.atlas.find(this.name);
+  },
+  
+  icons(){
+    return [
+      this.region
+    ];
+  },
+  
+  update(tile){
+    if (tile.entity.timer.get(0, 180) {
+        if (tile.entity.maxHealth < (wall.health*5)) {
+            tile.entity.maxHealth += ((wall.health*5)-wall.health)/160;
+            tile.entity.health += ((wall.health*5)-wall.health)/160;
+        };
+    };
+  }
+});
