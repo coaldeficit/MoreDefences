0470eff646e3a311e599337ef6df6af53e36caec
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-04-13 18:07:32 +0200

    Update communicator.js

--- scripts/units/air/swarmer/communicator.js
+++ scripts/units/air/swarmer/communicator.js
@@ -3,6 +3,6 @@ SwarmerT5.constructor = () => extend(UnitEntity, {});
 
 var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-messenger-ship"), Vars.content.getByName(ContentType.unit, "more-defences-ivyx-communicator-ship")]);
 Blocks.tetrativeReconstructor.upgrades.add(upgrade.toArray(UnitType));
-SwarmerT5.abilities.add(new UnitSpawnAbility(Vars.content.getByName(ContentType.unit, "more-defences-ivyx-hornet-ship"), 65, 0, 26));
-SwarmerT5.abilities.add(new UnitSpawnAbility(Vars.content.getByName(ContentType.unit, "more-defences-ivyx-bee-ship"), 25, 13, -16));
-SwarmerT5.abilities.add(new UnitSpawnAbility(Vars.content.getByName(ContentType.unit, "more-defences-ivyx-bee-ship"), 25, -13, -16));
+SwarmerT5.abilities.add(new UnitSpawnAbility(Vars.content.getByName(ContentType.unit, "more-defences-ivyx-hornet-ship"), 60*65, 0, 26));
+SwarmerT5.abilities.add(new UnitSpawnAbility(Vars.content.getByName(ContentType.unit, "more-defences-ivyx-bee-ship"), 60*25, 13, -16));
+SwarmerT5.abilities.add(new UnitSpawnAbility(Vars.content.getByName(ContentType.unit, "more-defences-ivyx-bee-ship"), 60*25, -13, -16));
