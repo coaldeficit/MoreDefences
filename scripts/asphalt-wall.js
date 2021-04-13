bcdb11e0766fd833b326f31525987a351b1e3417
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-04-13 08:43:49 +0200

    Update asphalt-wall.js

--- scripts/asphalt-wall.js
+++ scripts/asphalt-wall.js
@@ -1,24 +1,15 @@
 // to-do: actually make this code work (probably wont happen anytime in the next 600 irl days)
 
-const wall = extendContent(Wall, "asphalt-wall", {
-  load(){
-    this.region = Core.atlas.find(this.name);
-  },
-  
-  icons(){
-    return [
-      this.region
-    ];
-  }
+const wall = extend(Wall, "asphalt-wall", {
 });
 
 wall.buildType = () => extend(Wall.WallBuild, wall, {
    updateTile(){
      this.super$updateTile();
-     if (timer.get(0, 180)) {
+     if (this.timer.get(1, 180)) {
          if (this.maxHealth < (wall.health*5)) {
              this.maxHealth += ((wall.health*5)-wall.health)/160;
-             heal(((wall.health*5)-wall.health)/160);
+             this.health += ((wall.health*5)-wall.health)/160;
          };
      };
   }
