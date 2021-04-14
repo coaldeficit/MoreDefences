e6c7dc112c683751b76f678ff5a6d168735ae09e
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-04-14 14:31:50 +0200

    Update status-effects.js

--- scripts/experimental/status-effects.js
+++ scripts/experimental/status-effects.js
@@ -1,15 +1,15 @@
 // LH2 effect
 let liquidHydrogenEffect = extend(StatusEffect, "liquidHydrogenEffect", {
-    transitionDamage = 18
+    //transitionDamage = 18
     isHidden(){
       return false
     },
-    init(() -> {
+    /*init(() -> {
       affinity(StatusEffects.blasted, ((unit, time, newTime, result) -> {
         unit.damagePierce(transitionDamage);
         result.set(liquidHydrogenEffect, time);
       }));
-      /*affinity(StatusEffects.burning, ((unit, time, newTime, result) -> {
+      affinity(StatusEffects.burning, ((unit, time, newTime, result) -> {
         unit.damagePierce(transitionDamage / 1.5);
         result.set(liquidHydrogenEffect, time);
         result.set(StatusEffects.burning, time + newTime);
@@ -18,11 +18,11 @@ let liquidHydrogenEffect = extend(StatusEffect, "liquidHydrogenEffect", {
         unit.damagePierce(transitionDamage / 1.5);
         result.set(liquidHydrogenEffect, time);
         result.set(StatusEffects.melting, time + newTime);
-      }));*/
-    });
+      }));
+    });*/
 });
 liquidHydrogenEffect.speedMultiplier = 0.6;
 liquidHydrogenEffect.healthMultiplier = 0.7;
 liquidHydrogenEffect.localizedName = 'Hydrogenated';
 liquidHydrogenEffect.effect = Fx.freezing;
-liquidHydrogenEffect.color  = '#7a9a9B';
+liquidHydrogenEffect.color  = Color.valueOf("#7a9a98");
