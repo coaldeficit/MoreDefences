49dbf425f6e41a1aaf94d54b0df68db2f05f20c5
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-04-14 13:02:28 +0200

    Update status-effects.js

--- scripts/status-effects.js
+++ scripts/status-effects.js
@@ -1,8 +1,25 @@
 // LH2 effect
 let liquidHydrogenEffect = extend(StatusEffect, "liquidHydrogenEffect", {
+    let transitionDamage = 18
     isHidden(){
-        return false
+      return false
     },
+    init(() -> {
+      affinity(StatusEffects.blasted, ((unit, time, newTime, result) -> {
+        unit.damagePierce(transitionDamage);
+        result.set(liquidHydrogenEffect, time);
+      }));
+      affinity(StatusEffects.burning, ((unit, time, newTime, result) -> {
+        unit.damagePierce(transitionDamage / 1.5);
+        result.set(liquidHydrogenEffect, time);
+        result.set(StatusEffects.burning, time + newTime);
+      }));
+      affinity(StatusEffects.melting, ((unit, time, newTime, result) -> {
+        unit.damagePierce(transitionDamage / 1.5);
+        result.set(liquidHydrogenEffect, time);
+        result.set(StatusEffects.melting, time + newTime);
+      }));
+    });
 });
 liquidHydrogenEffect.speedMultiplier = 0.6;
 liquidHydrogenEffect.healthMultiplier = 0.7;
