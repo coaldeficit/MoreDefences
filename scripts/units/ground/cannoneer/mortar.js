faba212eb7bda4c0586530bb61ca632cf4016ed4
7rodo <7rodovje7878@gmail.com>
2021-04-15 17:06:13 +0100

    Update mortar.js

--- scripts/units/ground/cannoneer/mortar.js
+++ scripts/units/ground/cannoneer/mortar.js
@@ -3,4 +3,4 @@ CannonT4.constructor = () => extend(LegsUnit, {});
 CannonT4.immunities.add(StatusEffects.corroded);
 
 var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "more-defences-ivyx-ordnance-mech"), Vars.content.getByName(ContentType.unit, "more-defences-ivyx-mortar-mech")]);
-Blocks.multiplicativeReconstructor.upgrades.add(upgrade.toArray(UnitType));
+Blocks.exponentialReconstructor.upgrades.add(upgrade.toArray(UnitType));
