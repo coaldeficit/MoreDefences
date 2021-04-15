2f90e20775ce9c20d2124ccd19cdc7f084379ccb
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-04-15 11:18:24 +0200

    Update status-effects.js

--- scripts/experimental/status-effects.js
+++ scripts/experimental/status-effects.js
@@ -9,3 +9,7 @@ let liquidHydrogenEffect = extend(StatusEffect, "liquidHydrogenEffect", {
     effect: Fx.freezing,
     color: Color.valueOf("#7a9a98"),
 });
+liquidHydrogenEffect.affinity(StatusEffects.blasted, ((unit, time, newTime, result) => {
+    unit.damagePierce(18);
+    result.set(liquidHydrogenEffect, time);
+}));
