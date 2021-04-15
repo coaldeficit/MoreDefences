e0183449fb7babeb85148b8d3c2de5a6a936f414
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-04-15 11:55:44 +0200

    Create status-effects.js

---
+++ scripts/status-effects.js
@@ -0,0 +1,23 @@
+// LH2 effect
+let liquidHydrogenEffect = extend(StatusEffect, "liquidHydrogenEffect", {
+    isHidden(){
+      return false
+    },
+    speedMultiplier: 0.6,
+    healthMultiplier: 0.7,
+    localizedName: 'Hydrogenated',
+    effect: Fx.freezing,
+    color: Color.valueOf("#7a9a98"),
+});
+liquidHydrogenEffect.affinity(StatusEffects.blasted, ((unit, time, newTime, result) => {
+    unit.damagePierce(18);
+    result.set(liquidHydrogenEffect, time);
+}));
+liquidHydrogenEffect.affinity(StatusEffects.burning, ((unit, time, newTime, result) => {
+    unit.damagePierce(8);
+    result.set(StatusEffects.burning, newTime + time);
+}));
+liquidHydrogenEffect.affinity(StatusEffects.melting, ((unit, time, newTime, result) => {
+    unit.damagePierce(8);
+    result.set(StatusEffects.melting, newTime + time);
+}));
