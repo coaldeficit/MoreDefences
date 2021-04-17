82257bbc51716d5f077737460e9e24321ede88a2
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-04-17 16:51:20 +0200

    that did not go as planned

--- scripts/unmineable-ores.js
+++ scripts/unmineable-ores.js
@@ -1,22 +1,13 @@
 // Primary Color Minerals
 let vanadinite = extend(DoubleOverlayFloor, "vanadinite", {
-  localizedName: Vanadinite,
-  variants: 3,
-  playerUnmineable: true,
   mapColor: Color.valueOf("#ED3F00"),
   useColor: true
 });
 let adamite = extend(DoubleOverlayFloor, "adamite", {
-  localizedName: Adamite,
-  variants: 4,
-  playerUnmineable: true,
   mapColor: Color.valueOf("#A7BC40"),
   useColor: true
 });
 let chalcanthite = extend(DoubleOverlayFloor, "chalcanthite", {
-  localizedName: Chalcanthite,
-  variants: 3,
-  playerUnmineable: true,
   mapColor: Color.valueOf("#24B6FC"),
   useColor: true
 });
