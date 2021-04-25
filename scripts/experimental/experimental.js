4c79fd0845225afb4f06ef659eda5f5b6490db91
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-04-25 15:38:01 +0200

    Update experimental.js

--- scripts/experimental/experimental.js
+++ scripts/experimental/experimental.js
@@ -1,2 +1,3 @@
 require("more-defences-ivyx/experimental/exp-status-effects")
-require("more-defences-ivyx/experimental/vanadinite-extractor")
+//require("more-defences-ivyx/experimental/vanadinite-extractor")
+require("more-defences-ivyx/experimental/benzene-distiller")
