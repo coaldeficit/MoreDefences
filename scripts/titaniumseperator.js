95a9b5aa51c663d47d256d145d87f15c257b7ce9
7rodo <7rodovje7878@gmail.com>
2021-05-04 20:24:37 +0100

    Update titaniumseperator.js

--- scripts/titaniumseperator.js
+++ scripts/titaniumseperator.js
@@ -1,16 +1,20 @@
 const ts = extend(Separator, "titaniumseperator", {
   load(){
     this.region = Core.atlas.find(this.name);
+    this.spinnerRegion = Core.atlas.find(`${this.name}-spinner`);
   },
   
   icons(){
     return [
       this.region,
+      this.spinnerRegion,
     ];
   }
 });
 ts.buildType = () => extend(Separator.SeparatorBuild, ts, {
   draw(){
     Draw.rect(ts.region, this.x, this.y);
+    Draw.rect(ts.spinnerRegion, this.x, this.y, this.totalProgress * this.spinnerSpeed);
+
   }
 });
