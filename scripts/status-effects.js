48f448ef6a1104e4cdd770e936443a5f57f81652
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-06-19 16:30:26 +0200

    fdgfdgbgvfdgf
    frick this

--- scripts/status-effects.js
+++ scripts/status-effects.js
@@ -8,23 +8,18 @@ let liquidHydrogenEffect = extend(StatusEffect, "liquid-hydrogen-status-effect",
     localizedName: 'Hydrogenated',
     effect: Fx.freezing,
     color: Color.valueOf("#7a9a9B"),
-    init(() => {
-        affinity(StatusEffects.blasted, ((unit, time, newTime, result) => {
-            unit.damagePierce(18);
-            result.set(StatusEffects.blasted, newTime + time);
-            result.set(liquidHydrogenEffect, time);
-        }));
-    });
 });
+liquidHydrogenEffect.affinity(StatusEffects.blasted, ((unit, time, newTime, result) => {
+    unit.damagePierce(18);
+    result.set(liquidHydrogenEffect, time);
+}));
 liquidHydrogenEffect.affinity(StatusEffects.burning, ((unit, time, newTime, result) => {
     unit.damagePierce(8);
-    result.set(StatusEffects.burning, result.time + time);
-    result.set(liquidHydrogenEffect, time);
+    result.set(StatusEffects.burning, newTime + time);
 }));
 liquidHydrogenEffect.affinity(StatusEffects.melting, ((unit, time, newTime, result) => {
     unit.damagePierce(8);
     result.set(StatusEffects.melting, newTime + time);
-    result.set(liquidHydrogenEffect, time);
 }));
 
 module.exports = {
