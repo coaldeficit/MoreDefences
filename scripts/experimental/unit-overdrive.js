dad69fc5c09e490951c3eaece678437b4b3fbd5d
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-06-20 22:10:19 +0200

    Update unit-overdrive.js

--- scripts/experimental/unit-overdrive.js
+++ scripts/experimental/unit-overdrive.js
@@ -1,13 +1,29 @@
-const beam = new Effect(10, e => {
-  Draw.color(Color.valueOf("#feb380"))
+const beam = new Effect(30, e => {
+  Draw.color(Pal.accent) // Color.valueOf("#feb380")
   Lines.stroke(3 * e.fout())
   Lines.line(e.x, e.y, e.data.x, e.data.y)
   Lines.stroke(1 * e.fout())
 });
 
-const beamendthing = new Effect(10, e => {
-  Draw.color(Color.valueOf("#feb380"))
+const beamendthing = new Effect(30, e => {
+  Draw.color(Pal.accent) // Color.valueOf("#feb380")
   Fill.circle(e.x, e.y, e.fout() * e.data.hitSize + 1)
+});
+
+let decoLightningLightning = extend(LightningBulletType, {
+  damage: 6,
+  lightningLength: 10,
+  lightningLengthRand: 5,
+  lightningColor: Pal.accent
+});
+let decoLightning = extend(BasicBulletType, {
+  speed: 0.001,
+  lifetime: 1,
+  damage: 0,
+  height: 0.1,
+  width: 0.1,
+  fragBullets: 7,
+  fragBullet: decoLightningLightning
 });
 
 function dst(x1, x2, y1, y2) {
@@ -18,39 +34,58 @@ function dst(x1, x2, y1, y2) {
   return dx + dy;
 };
 
-const boostT1 = extend(Block, "unit-overdrive", {
-  buildVisibility: BuildVisibility.shown,
-  size: 2,
-  update: true,
-  destructible: true,
-  solid: true,
+let boostT1 = extend(Block, "unit-overdrive", {
   health: 120,
+  size: 2,
+  solid: true,
+  destructible: true,
+  update: true,
+  buildVisibility: BuildVisibility.shown,
+  hasPower: true,
+  consumesPower: true,
+  /*getOverdriveTimer(){
+    return this.overdrivetimer; // thanks to QmelZ for helping with the bar thing (or well, trying to help with the bar thing since it ended up not working anyway)
+  },
+  setBars(){
+    this.super$setBars();
+    this.bars.add("charge", func(e =>
+      new Bar(
+        prov(() => "Charge"),
+        prov(() => Pal.accent),
+        floatp(() => 1 - (e.getOverdriveTimer() / (60*35)))
+      ))
+    )
+  }*/
 });
+boostT1.consumes.power(1850 / 60);
 
 boostT1.buildType = () => extend(Building, {
   update(){
     this.super$update();
     if (this.overdrivetimer == undefined) this.overdrivetimer = 60 * 35;
     if (this.overdrivetargetcount == undefined) this.overdrivetargetcount = 10;
-    if (this.overdrivetimer > 0 && this.unitsdetected == true) this.overdrivetimer--;
     if (this.overdrivetimer < 60 * 35 && this.unitsdetected == false) this.overdrivetimer++;
+    if (this.overdrivetimer > 0 && this.unitsdetected == true) this.overdrivetimer--;
     this.unitsdetected = false
+    if (this.power.status === 1) {
     Groups.unit.each(unit => {
       if(unit.team == this.team){
          if(dst(this.x, unit.x, this.y, unit.y) < 7 * 10 ){
            this.unitsdetected = true
            if (this.overdrivetimer <= 0 && this.overdrivetargetcount > 0) {
-             beam.at(this.x, this.y, 0, unit);
-             beamendthing.at(unit.x, unit.y, 0, unit);
+             beam.at(unit.x, unit.y, 0, this);
+             beamendthing.at(unit.x, unit.y, 0, this);
              unit.apply(StatusEffects.overdrive, 9999999);
              this.overdrivetargetcount--;
              if (this.overdrivetargetcount == 0) {
                this.overdrivetimer = 60 * 35;
                this.overdrivetargetcount = 10
+	       decoLightning.create(unit, this.team, this.x, this.y, 0)
              };
 	   };
          };
       };
     });
+    }
   }
 });
