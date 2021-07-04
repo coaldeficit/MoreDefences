068d3d43c80de5905bfbbf7f8f4c56bbcb379bf2
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-07-05 00:22:34 +0200

    havoc anti-cheat
    havoc is now invincible for 4 seconds after it spawns to prevent cheesing it by just placing an oil filled liquid tank in the middle of the enemy spawn right before the wave with havoc in it

--- scripts/units/bosses/havoc.js
+++ scripts/units/bosses/havoc.js
@@ -8,6 +8,17 @@ const Boss1 = extend(UnitType, "havoc", {
 });
 
 
-Boss1.constructor = () => extend(MechUnit, {});
+Boss1.constructor = () => extend(MechUnit, {
+	update(){
+		this.super$update();
+		if (this.antispawnkillTimer == null) {
+			this.antispawnkillTimer = 0;
+		}
+		this.antispawnkillTimer += 1;
+		if (this.antispawnkillTimer <= (4*60)) {
+			this.health = 13000;
+		}
+	},
+});
 Boss1.immunities.add(StatusEffects.unmoving);
 Boss1.immunities.add(StatusEffects.disarmed);
