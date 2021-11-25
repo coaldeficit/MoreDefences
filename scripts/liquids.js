6b9076ef1be9b2a29efab9e1d9dc8464538038ba
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-11-25 08:09:44 +0100

    Update liquids.js

--- scripts/liquids.js
+++ scripts/liquids.js
@@ -1,5 +1,5 @@
 const statusMD = require("more-defences-ivyx/status-effects")
 // LH2
 let liquidHydrogen = extend(Liquid, "liquid-hydrogen", {
-    effect: StatusEffects.freezing
+    effect: statusMD.liquidHydrogenEffect
 });
