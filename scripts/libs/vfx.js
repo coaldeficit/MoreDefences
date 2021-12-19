19dcd6bcb2db24321c56449e64960fd8113b7080
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-12-19 02:07:11 +0100

    avalanche bomb effect
    this took me like an hour to make for some reason

--- scripts/libs/vfx.js
+++ scripts/libs/vfx.js
@@ -10,8 +10,30 @@ const irradiatedstatus = new Effect(30, e => {
   Draw.color(Color.valueOf("#a68467"))
   Fill.circle(e.x, e.y, e.fout() * 0.75);
 });
+const freezeBombWeak = new Effect(30, e => {
+  Draw.color(Color.valueOf("#6ecdec"));
+  Lines.stroke(e.fout() * 2);
+  let circleRad = 4 + e.finpow() * 96;
+  Lines.circle(e.x, e.y, circleRad);
+  Draw.color(Color.valueOf("#6ecdec"));
+  for(let i = 0; i < 3; i++){
+    Drawf.tri(e.x, e.y, 6, 70 * e.fout(), (i*120)-90);
+  }
+  for(let i = 0; i < 3; i++){
+    Drawf.tri(e.x, e.y, 6, 45 * e.fout(), (i*120)+90);
+  }
+  Draw.color();
+  for(let i = 0; i < 3; i++){
+    Drawf.tri(e.x, e.y, 3, 25 * e.fout(), (i*120)-90);
+  }
+  for(let i = 0; i < 3; i++){
+    Drawf.tri(e.x, e.y, 3, 12.5 * e.fout(), (i*120)+90);
+  }
+  Drawf.light(e.x, e.y, circleRad * 1.6, Pal.heal, e.fout());
+});
 
 module.exports = {
     lh2status: lh2status,
     irradiatedstatus: irradiatedstatus,
+    freezeBombWeak: freezeBombWeak,
 };
