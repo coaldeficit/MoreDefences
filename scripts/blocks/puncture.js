187f071de517cada3aab0bafed8845dcc6c995ba
CD <159595900+coaldeficit@users.noreply.github.com>
2025-04-27 01:09:23 +0200

    plast puncture real

--- scripts/blocks/puncture.js
+++ scripts/blocks/puncture.js
@@ -22,16 +22,16 @@ let titaMissile = extend(MissileBulletType, {
   trailChance: 0,
   update(b) {
     if (Math.round(b.time) >= 3) {
-        if (!Vars.world.floorWorld(b.x, b.y).isLiquid) {
-            b.remove()
-        }
+      if (!Vars.world.floorWorld(b.x, b.y).isLiquid) {
+        b.remove()
+      }
     }
     this.super$update(b);
   },
   hitEntity(b, entity, health) {
     let crit = false
     if (entity instanceof Unit) {
-      if (entity.maxHealth - health < 150) {
+      if (entity.maxHealth - health < 75) {
         b.damage *= 11
         crit = true
         b.data = "crit"
@@ -45,7 +45,69 @@ let titaMissile = extend(MissileBulletType, {
     }
   }
 })
-// todo: plast ammo
+let plastMissile = extend(MissileBulletType, {
+  speed: 6.667,
+  damage: 28,
+  splashDamage: 16,
+  splashDamageRadius: 32,
+  lifetime: 36,
+  homingPower: 0.035*(2/3),
+  homingRange: 150,
+  collidesAir: false,
+  backColor: Color.valueOf("CBD97F"),
+  frontColor: Color.valueOf("ffffff"),
+  trailColor: Color.valueOf("CBD97F"),
+  trailWidth: 2,
+  trailLength: 13.5,
+  trailChance: 0,
+  maxDamageFraction: 0.5,
+  intervalBullets: 1,
+  bulletInterval: 8,
+  intervalDelay: 4,
+  intervalRandomSpread: 10,
+  intervalBullet: extend(BasicBulletType, {
+    speed: 2,
+    damage: 3,
+    pierceArmor: true,
+    lifetime: 64,
+    frontColor: Color.valueOf("ffffff"),
+    backColor: Color.valueOf("CBD97F"),
+    height: 10,
+    width: 5,
+    update(b) {
+      if (!Vars.world.floorWorld(b.x, b.y).isLiquid) {
+        b.remove()
+      }
+      this.super$update(b);
+    },
+  }),
+  update(b) {
+    if (Math.round(b.time) >= 4.5) {
+      if (!Vars.world.floorWorld(b.x, b.y).isLiquid) {
+        b.remove()
+      }
+    }
+    this.super$update(b);
+  },
+  hitEntity(b, entity, health) {
+    let crit = false
+    let dmg = b.damage
+    if (entity instanceof Unit) {
+      if (entity.maxHealth - health < 120) {
+        b.damage *= 8.64
+        crit = true
+        b.data = "crit"
+      }
+    }
+    this.super$hitEntity(b, entity, health);
+    if (crit) {
+      b.damage = dmg
+      b.data = null
+      crit = false
+    }
+  }
+})
 puncture.ammo(
-  Items.titanium, titaMissile
+  Items.titanium, titaMissile,
+  Items.plastanium, plastMissile
 );
