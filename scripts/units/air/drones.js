51fcc0c5cd447bc1835a97f6cac916310748d3c0
CD <159595900+coaldeficit@users.noreply.github.com>
2025-07-08 22:16:33 +0200

    troni

--- scripts/units/air/drones.js
+++ scripts/units/air/drones.js
@@ -1,2 +1,4 @@
 const AssDrone = extend(UnitType, "assault-drone", {});
 AssDrone.constructor = () => extend(TimedKillUnit, {});
+const AssEmblyDrone = extend(UnitType, "autoassembly-drone", {});
+AssEmblyDrone.constructor = () => extend(BuildingTetherPayloadUnit, {});
