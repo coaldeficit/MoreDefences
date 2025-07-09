933b732d29400db78faeda5d3e8bc9bab7fd4c72
CD <159595900+coaldeficit@users.noreply.github.com>
2025-07-09 03:02:11 +0200

    troni damage groups (lmao)

--- scripts/units/air/drones.js
+++ scripts/units/air/drones.js
@@ -1,4 +1,14 @@
+const dmgroups = require("md3/units/damagegroups")
 const AssDrone = extend(UnitType, "assault-drone", {});
 AssDrone.constructor = () => extend(TimedKillUnit, {});
-const AssEmblyDrone = extend(UnitType, "autoassembly-drone", {});
+const tronigroups = [
+  'basic','malis','simple','unplated','attractor'
+]
+const AssEmblyDrone = extend(UnitType, "autoassembly-drone", {
+  setStats() {
+	this.super$setStats()
+	this.stats.add(dmgroups.groupStatUnit, StatValues.string(dmgroups.generateGroupStatUnit(tronigroups),{}))
+  }
+});
 AssEmblyDrone.constructor = () => extend(BuildingTetherPayloadUnit, {});
+dmgroups.addToGroups(tronigroups,AssEmblyDrone)
