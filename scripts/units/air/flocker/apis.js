4607f3db99a1e39269f9eeb19f6224e73dc1d265
CD <159595900+coaldeficit@users.noreply.github.com>
2025-12-09 20:49:28 +0100

    apis rework finally

--- scripts/units/air/flocker/apis.js
+++ scripts/units/air/flocker/apis.js
@@ -1,5 +1,83 @@
+const bulLib = require("md3/libs/bulletlib")
 const FlockT2 = extend(UnitType, "apis-ship", {});
 FlockT2.constructor = () => extend(UnitEntity, {});
+
+const apisGun = extend(Weapon, {
+  name: "md3-apis-gun",
+  y: 2,
+  x: 5,
+  top: true,
+  rotate: true,
+  rotateSpeed: 5,
+  inaccuracy: 5,
+  reload: 13,
+  shootSound: Sounds.shootSalvo,
+  alternate: false,
+  bullet: bulLib.makeBullet({
+    type: BasicBulletType,
+    sprite: "circle-bullet",
+    speed: 6,
+    lifetime: 20,
+    damage: 25,
+    lightning: 2,
+    lightningLength: 5,
+    lightningLengthRand: 2,
+    lightningDamage: 12,
+    shrinkX: 0,
+    shrinkY: 0,
+    width: 4,
+    height: 4,
+    frontColor: Color.white,
+    backColor: Pal.surge
+  }),
+});
+const flare = extend(MissileUnitType, "apis-ship-frag", {
+  targetAir: false,
+  speed: 2,
+  accel: 0,
+  drag: 1/25,
+  lifetime: 360,
+  outlineColor: Pal.darkOutline,
+  health: 260,
+  armor: 2,
+  loopSound: Sounds.none,
+  rotateSpeed: 0,
+  trailLength: 0,
+  engineSize: 0,
+  physics: false,
+  targetPriority: 0,
+});
+flare.controller = () => extend(FlyingAI, {})
+flare.constructor = () => extend(TimedKillUnit, {});
+const flareWeapon = extend(Weapon, {
+  name: "md3-generic-bomber-weapon",
+  y: 0,
+  x: 0,
+  top: true,
+  inaccuracy: 30,
+  reload: 60,
+  shootSound: Sounds.none,
+  shootCone: 361,
+  shootOnDeath: true,
+  shoot: new ShootSpread(3,120),
+  controllable: false,
+  aiControllable: false,
+  display: false,
+  bullet: bulLib.makeBullet({
+    type: BulletType,
+    shootEffect: Fx.shootBig,
+    smokeEffect: Fx.shootBigSmoke2,
+    speed: 0,
+    keepVelocity: false,
+    collides: false,
+    spawnUnit: flare,
+    rangeOverride: 1
+  }),
+});
+FlockT2.weapons.add(
+  apisGun,
+  flareWeapon
+);
 
 // this will be removed in 3.3.0! update your maps before then!!!
 const FlockT2Placeholder = extend(UnitType, "bee-ship", {
