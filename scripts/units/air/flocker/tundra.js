b137b6a41564f22bb79fb32c548263128d35ed24
CD <159595900+coaldeficit@users.noreply.github.com>
2025-12-17 02:29:35 +0100

    tundra rework

--- scripts/units/air/flocker/tundra.js
+++ scripts/units/air/flocker/tundra.js
@@ -1,26 +1,52 @@
 const vfx = require("md3/libs/vfx")
 const bulLib = require("md3/libs/bulletlib")
-const rng = require("md3/libs/rng")
 const blockcheck = require("md3/libs/blockcheck")
 const icewall = require("md3/blocks/frozen-wall")
 const FlockT5 = extend(UnitType, "tundra-ship", {});
 FlockT5.constructor = () => extend(UnitEntity, {});
 
-const tundraBombFrag = extend(ArtilleryBulletType, {
-  speed: 2,
-  spin: 6,
-  width: 10,
-  height: 10,
-  damage: 15,
-  splashDamage: 35,
-  splashDamageRadius: 24,
+const tundraBombFrag = extend(ShrapnelBulletType, {
+  length: 125,
+  width: 30,
+  damage: 120,
+  status: StatusEffects.freezing,
+  statusDuration: 720,
+  serrations: 18,
+  serrationSpacing: 6.67,
+  serrationWidth: 4,
+  serrationLenScl: 2.5,
+  serrationSpaceOffset: 50,
+  serrationFadeOffset: 0,
+  hitLarge: true,
+  lifetime: 30,
   hitEffect: Fx.flakExplosion,
-  despawnEffect: Fx.shockwave,
-  frontColor: Color.valueOf("#ffffff"),
-  backColor: Color.valueOf("#6ecdec"),
-  collide: false,
-  sprite: 'md3-d-bomb',
-  hitShake: 2,
+  fromColor: Color.valueOf("#ffffff"),
+  toColor: Color.valueOf("#5ce7ff"),
+  hitTile(b, build, x, y, initialHealth, direct) {
+    this.super$hitTile(b, build, x, y, initialHealth, direct);
+    if (true) {
+      if (build.health <= build.maxHealth*0.3) {
+        build.damage(b.owner.team, build.maxHealth+1)
+      }
+      if (build.block.size >= 1 && build.block.size <= 4 && build.dead) {
+        build.addPlan(true)
+        let block = build.tile
+        let wallToPlace = icewall.frozenWall
+        switch (build.block.size) {
+          case 2:
+            wallToPlace = icewall.frozenWallL
+            break
+          case 3:
+            wallToPlace = icewall.frozenWallH
+            break
+          case 4:
+            wallToPlace = icewall.frozenWallG
+            break
+        }
+        block.setNet(wallToPlace, b.owner.team, 0)
+      }
+    }
+  }
 });
 const tundraBomb = extend(BasicBulletType, {
   hitSound: Sounds.explosionQuad,
@@ -31,16 +57,16 @@ const tundraBomb = extend(BasicBulletType, {
   width: 30,
   height: 30,
   frontColor: Color.valueOf("#ffffff"),
-  backColor: Color.valueOf("#6ecdec"),
+  backColor: Color.valueOf("#5ce7ff"),
   sprite: 'md3-plus-bomb',
   spin: 2,
   shrinkX: 0.45,
   shrinkY: 0.45,
   hitShake: 9,
   splashDamageRadius: 96,
-  splashDamage: 300,
+  splashDamage: 180,
   status: StatusEffects.freezing,
-  statusDuration: 720,
+  statusDuration: 300,
   speed: 4,
   drag: 4,
   collides: false,
@@ -48,7 +74,7 @@ const tundraBomb = extend(BasicBulletType, {
   lifetime: 140,
   keepVelocity: false,
   hittable: false,
-  fragBullets: 10,
+  fragBullets: 7,
   fragBullet: tundraBombFrag,
   range(){ // note to anyone reading: always override the range function for any bomb-type projectiles so that the unit ai actually uses them
     return 150
@@ -58,8 +84,7 @@ const tundraBomb = extend(BasicBulletType, {
     vfx.freezeBombExplosion.at(b.x, b.y);
     vfx.freezeBomb.at(b.x, b.y);
     vfx.b135impactShockwave.at(b.x, b.y);
-    rng.setIndex(4897)
-    blockcheck.iterateSquareCenter(Math.round(b.x/8),Math.round(b.y/8),24,24,(other => {
+    /*blockcheck.iterateSquareCenter(Math.round(b.x/8),Math.round(b.y/8),24,24,(other => {
       let rngValue = rng.randomUnsynced()
       if (other.build != null) {
         let o = other.block()
@@ -78,13 +103,13 @@ const tundraBomb = extend(BasicBulletType, {
           }
         }
       }
-    }));
+    }));*/
   }
 });
 
 const bombCannon = extend(Weapon, {
   name: "md3-generic-bomber-weapon",
-  minShootVelocity: 0.25,
+  minShootVelocity: 0.01,
   mirror: false,
   x: 0,
   shootY: 0,
@@ -97,8 +122,138 @@ const bombCannon = extend(Weapon, {
   autoTarget: true
 });
 
+const underdriveWeapon = extend(Weapon, { // CHANGING THIS DOES NOT AFFECT THE DISPLAYED STATS
+  name: "md3-generic-bomber-weapon",
+  mirror: false,
+  x: 0,
+  shootY: 0,
+  reload: 10,
+  shootCone: 361,
+  shootSound: Sounds.none,
+  alwaysShooting: true,
+  display: false,
+  bullet: extend(ExplosionBulletType, {
+    splashDamage: -1,
+    splashDamageRadius: -1,
+    rangeOverride: 6,
+    killShooter: false,
+    shootEffect: Fx.none,
+    smokeEffect: Fx.none,
+    hitEffect: Fx.none,
+    despawnEffect: Fx.none,
+    collides: false,
+    hittable: false,
+    absorbable: false,
+    damage: 0,
+    despawned(b) {
+      blockcheck.iterateSquareCenter(Math.round(b.owner.x/8),Math.round(b.owner.y/8),24,24,(other => {
+        if (other.build != null && other.build.team != b.owner.team) {
+          other.build.applySlowdown(0.667, 30)
+        }
+      }));
+    }
+  }),
+});
+const underdriveDeco1 = extend(SuppressionFieldAbility, {
+  active: false,
+  y: 39.5,
+  x: 22.25,
+  color: Color.valueOf("#5ce7ff"),
+  particleColor: Color.valueOf("#5ce7ff"),
+  effectColor: Color.valueOf("#5ce7ff"),
+  particleLen: 5,
+  orbRadius: 3,
+  init() {
+    this.display = true
+  },
+  getBundle() {return 'ability.md3-underdrive'},
+  addStats(t) {
+    t.add(Core.bundle.get(this.getBundle() + ".description")).wrap().width(this.descriptionWidth)
+    t.row()
+    t.add(Core.bundle.format("bullet.range", 12)); // CHANGING THESE DOES NOT AFFECT THE FUNCTION OF THE ABILITY
+    t.row()
+    t.add(this.abilityStat("md3-underdrive-decrease", 33.3));
+    t.row()
+    t.add(this.abilityStat("duration", 0.5));
+  }
+})
+const underdriveDeco2 = extend(SuppressionFieldAbility, {
+  active: false,
+  y: 39.5,
+  x: -22.25,
+  color: Color.valueOf("#5ce7ff"),
+  particleColor: Color.valueOf("#5ce7ff"),
+  effectColor: Color.valueOf("#5ce7ff"),
+  particleLen: 5,
+  orbRadius: 3,
+  init() {
+    this.display = false
+  }
+})
+const underdriveDeco3 = extend(SuppressionFieldAbility, {
+  active: false,
+  y: -38,
+  x: 21,
+  color: Color.valueOf("#5ce7ff"),
+  particleColor: Color.valueOf("#5ce7ff"),
+  effectColor: Color.valueOf("#5ce7ff"),
+  particleLen: 5,
+  orbRadius: 3,
+  init() {
+    this.display = false
+  }
+})
+const underdriveDeco4 = extend(SuppressionFieldAbility, {
+  active: false,
+  y: -38,
+  x: -21,
+  color: Color.valueOf("#5ce7ff"),
+  particleColor: Color.valueOf("#5ce7ff"),
+  effectColor: Color.valueOf("#5ce7ff"),
+  particleLen: 5,
+  orbRadius: 3,
+  init() {
+    this.display = false
+  }
+})
+const underdriveDeco5 = extend(SuppressionFieldAbility, {
+  active: false,
+  y: -29.25,
+  x: 32.75,
+  color: Color.valueOf("#5ce7ff"),
+  particleColor: Color.valueOf("#5ce7ff"),
+  effectColor: Color.valueOf("#5ce7ff"),
+  particleLen: 5,
+  orbRadius: 3,
+  init() {
+    this.display = false
+  }
+})
+const underdriveDeco6 = extend(SuppressionFieldAbility, {
+  active: false,
+  y: -29.25,
+  x: -32.75,
+  color: Color.valueOf("#5ce7ff"),
+  particleColor: Color.valueOf("#5ce7ff"),
+  effectColor: Color.valueOf("#5ce7ff"),
+  particleLen: 5,
+  orbRadius: 3,
+  init() {
+    this.display = false
+  }
+})
+FlockT5.abilities.addAll(
+  underdriveDeco1,
+  underdriveDeco2,
+  underdriveDeco3,
+  underdriveDeco4,
+  underdriveDeco5,
+  underdriveDeco6
+)
+
 FlockT5.weapons.add(
   bombCannon,
+  underdriveWeapon
 );
 
 Blocks.tetrativeReconstructor.addUpgrade(
