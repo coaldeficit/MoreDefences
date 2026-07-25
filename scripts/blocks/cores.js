8f8ba1ed4212dc6d763f6cbf3f87bfc103aca3c0
coal deficit <159595900+coaldeficit@users.noreply.github.com>
2026-07-25 20:58:48 +0200

    wip core: atom

--- scripts/blocks/cores.js
+++ scripts/blocks/cores.js
@@ -1,3 +1,17 @@
+// serpulo
+let atom = extend(CoreBlock, "core-atom", {
+  load(){
+    this.super$load()
+    this.fullIconRegion = Core.atlas.find(this.name + "-full");
+  },
+  icons(){
+    return [
+      this.fullIconRegion
+    ];
+  }
+});
+
+// fieros
 let monoquark = extend(CoreBlock, "core-monoquark", {
   load(){
     this.super$load()
