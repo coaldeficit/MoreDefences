7683804d69814fee4a2f0ebfc592d363f3649479
coal deficit <159595900+coaldeficit@users.noreply.github.com>
2026-07-25 07:38:49 +0200

    balancing

--- scripts/blocks/juan.js
+++ scripts/blocks/juan.js
@@ -1,16 +1,23 @@
 const vfx = require("md3/libs/vfx")
 
-let juan = extend(ItemTurret, "juan", {});
+let juan = extend(ItemTurret, "juan", {
+  buildingFilter: {
+    get(u) {
+      return u.maxHealth >= 10000 && u.size >= 3
+    }
+  }
+});
 let blast = extend(RailBulletType, {
   shootEffect: vfx.juanBlast,
   smokeEffect: vfx.juanSmoke,
-  hitSound: Vars.tree.loadSound("md3-bigshot"),
+  hitSound: Vars.tree.loadSound("md3-bigshot"), // cungadero
   damage: 7000,
-  length: 160,
+  length: 240,
   ammoMultiplier: 1,
   status: StatusEffects.unmoving,
   statusDuration: 180,
   pierceArmor: true,
+  buildingDamageMultiplier: 0.5,
 })
 juan.ammoTypes.put(
   Items.blastCompound, blast
